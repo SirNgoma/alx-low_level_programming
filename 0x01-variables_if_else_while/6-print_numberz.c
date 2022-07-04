c2d7d478f9c4340aa8ac326118fb188b6ec47496
Themba <seconds.seconds1@gmail.com>
2022-07-04 13:22:42 -0700

    numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -3,15 +3,16 @@
 /**
  *  * main - create numbers one to nine 
  *   * Return: prints number one to nine 
- *    */
+*/
 
-int main() {
-	    
-	 int i;
-	     for (i = 48; i <= 57 ; i++ )
-		         {
-				             putchar(i);
-					         }
-	         
-	         return 0;
+int main(void)
+{
+	int i;
+	
+	for (i = 48; i <= 57 ; i++ )
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
 }
