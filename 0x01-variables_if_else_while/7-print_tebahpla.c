0ca44e387851fbfe2604f3b9e54b3a4728b1fac7
Themba <seconds.seconds1@gmail.com>
2022-07-04 20:41:44 -0700

    n7

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -3,19 +3,16 @@
 /**
  *  * main - create alphabets in reverse 
  *   * Return: prints lowercase alphabets in reverse 
- *    */
+ */
 
+int main(void)
+{
+	int r_alpha;
 
-int main() {
-	    
-	    int r_alpha;
-	        
-	        
-	        for (r_alpha = 122; r_alpha >= 97; r_alpha--)
-			    {
-				                putchar(r_alpha);
-						    
-						    }
-		    
-		    return 0;
+	for (r_alpha = 122; r_alpha >= 97; r_alpha--)
+	{
+		putchar(r_alpha);
+	}
+	putchar('\n');
+	return (0);
 }
