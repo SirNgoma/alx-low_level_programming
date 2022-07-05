12fb0ce7235b67b31d4dbbb33cebf32658fd9b03
Themba <seconds.seconds1@gmail.com>
2022-07-04 20:30:46 -0700

    number8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,26 +1,25 @@
 #include <stdio.h>
 
 /**
- *  * main - Hexa create Base 16 numbers 
- *   * Return: prints number 0 - 9 and alpha a-f
- *    */
+ * main - Hexa create Base16 numbe
+ * Return: prints number zero - nine and alpha a-f
+ */
 
 
 
-int main() {
-	    
-	    int base;
-	        char alph;
-		    
-		    
-		    for (base = 48; base <= 57; base++)
-			        {
-					            putchar(base);
-						        }
-		        for (alph = 97; alph <= 102; alph++)
-				        {
-						                putchar(alph);
-								        }
-			    
-			    return 0;
+int main(void)
+{
+	int base;
+	char alph;
+	
+	for (base = 48; base <= 57; base++)
+	{
+		putchar(base);
+	}
+	for (alph = 97; alph <= 102; alph++)
+	{
+		putchar(alph);
+	}
+	putchar('\n');
+	return (0);
 }
