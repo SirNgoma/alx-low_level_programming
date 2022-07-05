404d698a2483f19458c71e742edaae41c81ca1c5
Themba <seconds.seconds1@gmail.com>
2022-07-05 11:55:34 -0700

    lost

---
+++ 0x02-functions_nested_loops/printshit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * printshit - prints shit
+ *
+ * Return: nothing
+ */
+
+void printshit(void)
+{
+	_putchar('s');
+	_putchar('h');
+	_putchar('i');
+	_putchar('t');
+	_putchar('\n');
+}
