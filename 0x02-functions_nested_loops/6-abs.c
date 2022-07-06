40f3fbd969d1ad67480f9a013d531e4be1a1310c
Themba <seconds.seconds1@gmail.com>
2022-07-06 09:27:13 -0700

    n6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _abs -  abs main
+ *
+ * Return: abs of a number
+ */
+
+int _abs(int i)
+{
+	int r;
+
+	if (i<0)
+	{
+		r = i * -1;
+		return r;
+	}
+	else if (i>0)
+	{
+		r = i * 1;
+		return r;
+	}
+	else
+	{
+		return i;
+	}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -8,5 +8,6 @@ void print_alphabet(void);
 void print_alphabet_x10(void);
 int add(int num1, int num2);
 int print_last_digit(int i);
+int _abs(int);
 
 #endif
