cd382a20609e4c9b779a647ec6b3f40f04f5b4e6
Themba <seconds.seconds1@gmail.com>
2022-07-08 06:05:04 -0700

    fee

--- 0x04-more_functions_nested_loops/5-main.c
+++ 0x04-more_functions_nested_loops/5-main.c
@@ -1,12 +1,12 @@
-nclude "main.h"
+#include "main.h"
 
 /**
  *  * main - check the code
  *   *
  *    * Return: Always 0.
- *     */
+*/
 int main(void)
 {
-	    more_numbers();
-	        return (0);
+	more_numbers();
+	return (0);
 }
