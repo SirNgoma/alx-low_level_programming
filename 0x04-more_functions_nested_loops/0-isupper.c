a3f5f78003275b6420ea7b25e626d4b0e0cb03d4
Themba <seconds.seconds1@gmail.com>
2022-07-07 19:39:08 -0700

    g

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,21 +2,7 @@
 #include <stdio.h>
 
 /**
- *_isupper - check the code.
- * @c  : param
+ *  - check the code.
  * 
  * Return: nothing
 */
-
-int _isupper(int c)
-{
-	if(c >= 65 && c <= 90)
-	{
-		return (1);
-	}
-	else
-	{
-		return (0);
-	}
-}
-
