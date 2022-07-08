e92a61404508e5412a62a1f4c21e48b4f020c8e2
Themba <seconds.seconds1@gmail.com>
2022-07-08 09:17:17 -0700

    eish

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - multiplies
+ *
+ * @a: number 1
+ *
+ * @b: number 2
+ *
+ * Return: nothing
+ *
+ *
+ */
+
+int mul(int a, int b)
+{
+	int r;
+
+	r = a * b;
+	return (r);
+}
