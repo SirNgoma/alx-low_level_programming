2bc8c29584fcb639f040854326b12fee2c69cea4
Themba <seconds.seconds1@gmail.com>
2022-07-12 10:59:57 -0700

    yoo

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * * swap_int - check the code
+ * @*a: ffs vfg
+*@*b: hg gfd
+ *
+* Return: Always 0.
+ */
+void swap_int(int *a, int *b)
+{
+	int na = *a;
+
+	*a = *b;
+	*b = na;
+}
