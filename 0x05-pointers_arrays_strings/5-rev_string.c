dc1838026aa399ed8ae9df55de59abd4c28d7be3
Themba <seconds.seconds1@gmail.com>
2022-07-12 20:07:48 -0700

    yrs

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,29 @@
+
+
+
+
+
+
+
+void rev_string(char *s)
+{
+	int length, c;
+	   char *begin, *end, temp;
+	    
+	      length = _strlen(s);
+	         begin  = s;
+		    end    = s;
+		     
+		       for (c = 0; c < length - 1; c++)
+			             end++;
+		        
+		          for (c = 0; c < length/2; c++)
+				     {        
+					           temp   = *end;
+						         *end   = *begin;
+							       *begin = temp;
+							        
+							             begin++;
+								           end--;
+									      }
+}
