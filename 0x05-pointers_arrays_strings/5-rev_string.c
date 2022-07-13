ef280fabbf9def137938f284520a92e084866092
Themba <seconds.seconds1@gmail.com>
2022-07-12 21:13:54 -0700

    ges

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,29 +1,30 @@
+#include "main.h"
+#include <stdio.h>
 
-
-
-
-
+/*
+ * rev_string - hd
+ * @s: gg
+ * Return: ff
+ */
 
 
 void rev_string(char *s)
 {
 	int length, c;
-	   char *begin, *end, temp;
-	    
-	      length = _strlen(s);
-	         begin  = s;
-		    end    = s;
-		     
-		       for (c = 0; c < length - 1; c++)
-			             end++;
-		        
-		          for (c = 0; c < length/2; c++)
-				     {        
-					           temp   = *end;
-						         *end   = *begin;
-							       *begin = temp;
-							        
-							             begin++;
-								           end--;
-									      }
+	char *begin, *end, temp;
+
+	length = _strlen(s);
+	begin  = s;
+	end    = s;
+
+	for (c = 0; c < length - 1; c++)
+		end++;
+	for (c = 0; c < length / 2; c++)
+	{
+		temp   = *end;
+		*end   = *begin;
+		*begin = temp;
+		begin++;
+		end--;
+	}
 }
