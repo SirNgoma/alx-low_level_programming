cdb53dbfe785813c35cd6a2f072041683b6207be
themba <seconds.seconds1@gmail.com>
2022-12-08 11:30:55 -0800

    print* betth

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,11 +3,11 @@
 /**
  * main - printf
  *
- * Retun: always 0
+ * Return: always 0
  */
 
 int main(void)
 {
-printf(" with proper grammar, but the outcome is a piece of art,");
-return(0);
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
 }
