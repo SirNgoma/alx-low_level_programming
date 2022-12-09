27f7c97729f13c6112b65589872174827f84a12d
themba <seconds.seconds1@gmail.com>
2022-12-09 07:36:11 -0800

    all files

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - 
+ *
+ * Return: always zero
+ **/
+ 
+ int main(void)
+ {
+
+ }
---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+#include <stdlib.h>
+
+#include <time.h>
+
+/
+
+/**
+ *
+ *  * main -
+ *
+ *   *
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *     */
+
+int main(void)
+
+{
+
+		
+
+		
+
+		return (0);
+
+}
