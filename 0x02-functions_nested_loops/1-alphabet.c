a59b99447a7b2bcb61e22879c415961b5c863398
themba <seconds.seconds1@gmail.com>
2022-12-13 08:53:42 -0800

    get

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,16 +2,10 @@
 #include "main.h"
 
 /**
- * main - check the code
+ * print_alphabet  - check the code
  *
  * Return: Always 0.
  **/
-int main(void)
-{
-	print_alphabet();
-	return (0);
-}
-
 void print_alphabet(void)
 {
 	int l = 97;
