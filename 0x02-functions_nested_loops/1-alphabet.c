26e33e1c3c2beb3b2568290c2e3b7aff02eaec4c
themba <seconds.seconds1@gmail.com>
2022-12-13 08:30:41 -0800

    alpha 1st

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,10 +1,24 @@
 #include <stdio.h>
-#include <main.h>
+#include "main.h"
 
 /**
+ * main - check the code
  *
- *
- *
- *
+ * Return: Always 0.
  **/
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
 
+void print_alphabet(void)
+{
+	int l = 97;
+
+	while (l >= 122)
+	{
+		_putchar(l);
+	}
+	_putchar('\n');
+}
