1ae5f30ee89c9c211c1614a96b3fa1fc84622ab7
themba <seconds.seconds1@gmail.com>
2022-12-20 10:41:57 -0800

    now

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -9,5 +9,22 @@
 
 void puts_half(char *str)
 {
+	int i = 0;
+	int j = i / 2;
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	if (i % 2 == 1)
+	{
+		j++;
+	}
+	while (j < i)
+	{
+		_putchar(str[j]);
+		j++;
+	}
+	_putchar('\n');
 
 }
--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -10,5 +10,18 @@
 
 char *_strcpy(char *dest, char *src)
 {
+	int i = 0;
+
+	while (i >= 0)
+	{
+		dest[i] = src[i];
+
+		if (src[i] == '\0')
+		{
+			return (dest);
+		}
+		i++;
+	}
+	return (dest);
 
 }
