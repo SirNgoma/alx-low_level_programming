5aab1881cae9d13d9923486ef52055295559ee08
themba <seconds.seconds1@gmail.com>
2022-12-21 09:45:54 -0800

    h

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -12,14 +12,20 @@ void print_array(int *a, int n)
 {
 	int i;
 
-	for (i = 0; i < n; i++)
+	if (n > 0)
 	{
-		printf("%d", a[i]);
-		if (i != n - 1)
+		for (i = 0; i < n; i++)
 		{
-			printf(", ");
+			if (i != n - 1)
+			{
+				printf("%d, ", a[i]);
+			}
+			else
+			{
+				printf("%d", a[i]);
+			}
 		}
-		printf("\n");
 	}
+	printf("\n");
 }
 
