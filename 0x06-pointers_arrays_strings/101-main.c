c37bcf865855b4080f124301eca45b460f084b1c
themba <seconds.seconds1@gmail.com>
2022-12-23 19:39:05 -0800

    yess

--- 0x06-pointers_arrays_strings/101-main.c
+++ 0x06-pointers_arrays_strings/101-main.c
@@ -3,25 +3,20 @@
 /**
  *  * main - check the code
  *   *
- *    * Return: Always 0.
- *     */
-
-int _putchar(char c)
-{
-		return (write(1, &c, 1));
-}
+ *    * Return: Always 
+ **/
 
 int main(void)
 {
-	    print_number(98);
-	        _putchar('\n');
-		    print_number(402);
-		        _putchar('\n');
-			    print_number(1024);
-			        _putchar('\n');
-				    print_number(0);
-				        _putchar('\n');
-					    print_number(-98);
-					        _putchar('\n');
-						    return (0);
+	print_number(98);
+	_putchar('\n');
+	print_number(402);
+	_putchar('\n');
+	print_number(1024);
+	_putchar('\n');
+	print_number(0);
+	_putchar('\n');
+	print_number(-98);
+	_putchar('\n');
+	return (0);
 }
