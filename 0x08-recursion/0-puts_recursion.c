92684ed0e293e66d26c116324adbee86ed0abb92
themba <seconds.seconds1@gmail.com>
2023-01-05 07:43:50 -0800

    added

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,2 +1,14 @@
 #include "main.h"
 #include <stdio.h>
+
+/**
+ * _puts_recursion - prints a string
+ * @s:string to be printed
+ * Return: zero
+ **/
+
+void _puts_recursion(char *s)
+{
+	_putchar(s);
+	_putchar("\n");
+}
