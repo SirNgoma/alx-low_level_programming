2ec39f1e931178ca024d7a2afc10208c450709ea
themba <seconds.seconds1@gmail.com>
2023-01-05 08:53:12 -0800

    draft

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,2 +1,36 @@
 #include "main.h"
 #include <stdio.h>
+
+
+
+
+
+/**
+ *
+ *  * _print_rev_recursion - print reverse of a string
+ *
+ *   * @s: string
+ *
+ *    *
+ *
+ *     * Description: prints a string in reverse
+ *
+ *      * Return: always 0
+ *
+ *       */
+
+void _print_rev_recursion(char *s)
+
+{
+
+		if (*s)
+
+				{
+
+							_print_rev_recursion(s + 1);
+
+									_putchar(*s);
+
+										}
+
+}
--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,3 +1,45 @@
 #include "main.h"
 #include <stdio.h>
 
+
+/**
+ *
+ *  * factorial - returns the factorial
+ *
+ *   * @n: number to be calculated
+ *
+ *    * Description: Factorial  recursion function
+ *
+ *     * Return: the factorial of the number
+ *
+ *      */
+
+int factorial(int n)
+
+{
+
+		if (n == 0)
+
+				{
+
+							return (1);
+
+								}
+
+			else if (n > 0)
+
+					{
+
+								return (n * factorial(n - 1));
+
+									}
+
+				else
+
+						{
+
+									return (-1);
+
+										}
+
+}
