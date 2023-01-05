809ed151879388a0c849a9386a5e45bd5a725640
themba <seconds.seconds1@gmail.com>
2023-01-05 07:59:28 -0800

    added a

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -9,6 +9,15 @@
 
 void _puts_recursion(char *s)
 {
-	_putchar(s);
-	_putchar("\n");
+
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return (0);
+	}
+	else
+	{
+		_putchar(*s);
+		_puts_recursion(*s++);
+
 }
