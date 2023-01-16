9edf046f8b076cbb7f4daaaae71c6be8f679091c
themba <seconds.seconds1@gmail.com>
2023-01-16 11:00:28 -0800

    all first

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ * Return: Zero
+ **/
+
+int main()
+{
+	printf("%s", __FILE__);
+	return (0);
+}
---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,4 @@
+#include <stdio.h>
+
+#define ABS(x) (abs(x))
+
