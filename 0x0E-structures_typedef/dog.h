07aca2bfa0f0ee0583da9314c3d5d9749e1a5c4b
themba <seconds.seconds1@gmail.com>
2023-01-17 03:05:26 -0800

    s

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,52 +1,16 @@
-#ifndef _DOG_
-
-#define _DOG_
-
-
+#ifndef DOG
+#define DOG
 
 /**
- *
- *  * struct dog - My dog Information
- *
- *   * @name: name of the dog
- *
- *    * @age: age of the dog
- *
- *     * @owner: owner of the dog
- *
- *      *
- *
- *       * Description: My dog informations
- *
- *        */
+ * struct dog - Di
+ * @name: name
+ * @age: age
+ * @owner: owner
+ **/
 
-struct dog
-
-{
-
-		char *name;
-
-			float age;
-
-				char *owner;
-
+typedef struct dog {
+	char *name;
+	float age;
+	char *owner;
 };
-
-
-
-typedef struct dog dog_t;
-
-
-
-void init_dog(struct dog *d, char *name, float age, char *owner);
-
-void print_dog(struct dog *d);
-
-char *_strdup(char *str);
-
-dog_t *new_dog(char *name, float age, char *owner);
-
-void free_dog(dog_t *d);
-
-#endif
 
