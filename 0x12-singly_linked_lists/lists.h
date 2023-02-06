e11e35414c731a9b32d4555ff39eafb02c5561eb
themba <seconds.seconds1@gmail.com>
2023-02-06 09:54:04 -0800

    l

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,60 +1,30 @@
-#ifndef _LISTS                                                                                                                                         
-
-#define _LISTS                                                                                                                                         
+#ifndef _LISTS
+#define _LISTS
 
 #include <stdio.h>
-
 #include <stdlib.h>
-
 #include <stddef.h>
 
+size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);                                                                                                    
+list_t *add_node_end(list_t **head, const char *str);                                                                                                
+void free_list(list_t *head);
 
-                                                                                                                                                       
-
-size_t print_list(const list_t *h);                                                                                                                    
-
-size_t list_len(const list_t *h);                                                                                                                      
-
-list_t *add_node(list_t **head, const char *str);                                                                                                      
-
-list_t *add_node_end(list_t **head, const char *str);                                                                                                  
-
-void free_list(list_t *head);                                                                                                                          
-
-                                                                                                                                                       
-
-                                                                                                                                                       
-
-/**                                                                                                                                                    
- *
- *  * struct list_s - singly linked list                                                                                                                  
- *
- *   *                                                                                                                                                     
- *
- *    * @str: string - (malloc'ed string)                                                                                                                   
- *
- *     *                                                                                                                                                     
- *
- *      * @len: length of the string                                                                                                                          
- *
- *       *                                                                                                                                                     
- *
- *        * @next: points to the next node                                                                                                                      
- *
- *         *                                                                                                                                                     
- *
- *          * Description: singly linked list node structure                                                                                                      
- *
- *           **/                                                                                                                                                   
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed str
+ * @len: length of the string
+ * @next: points to the next no
+ * Description: singly linked list node structure           
+ **/                                                                                                       
 
                                                                                                                                                        
 
 typedef struct list_s
 {
-	char *str;                                                                                                                                     
-
-	unsigned int len;                                                                                                                              
-
+	char *str;                                                                                                                                   
+	unsigned int len;                                                                                                                            
 	struct list_s *next;
 } list_t;                                                                                                                                              
 
