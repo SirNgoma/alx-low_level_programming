16e2fdd1a55eb5deb409236f16c40c1389379288
themba <seconds.seconds1@gmail.com>
2023-02-07 09:45:52 -0800

    l

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -1,61 +1,34 @@
 #include <stdlib.h>
-
-#include "holberton.h"
+#include "main.h"
 
 /**
+ * array_range - creates an array of integers
  *
- *  * array_range - creates an array of integers
+ * @min: minimum integer
  *
- *   * @min: minimum integer
+ * @max: maximum integer
  *
- *    * @max: maximum integer
- *
- *     * Return: array of integers
- *
- *      */
+ * Return: array of integers
+ **/
 
 int *array_range(int min, int max)
-
 {
+	int *array;
+	int i, array_size;
 
-		int *array;
+	if (min > max)
+		return (NULL);
 
-			int i, array_size;
+	array_size = (max - min) + 1;
+	array = malloc(sizeof(int) * array_size);
 
+	if (array == NULL)
+		return (NULL);
 
-
-				if (min > max)
-
-							return (NULL);
-
-
-
-					array_size = (max - min) + 1;
-
-
-
-						array = malloc(sizeof(int) * array_size);
-
-
-
-							if (array == NULL)
-
-										return (NULL);
-
-
-
-								for (i = 0; i < array_size; i++)
-
-										{
-
-													array[i] = min;
-
-															min = min + 1;
-
-																}
-
-
-
-									return (array);
-
+	for (i = 0; i < array_size; i++)
+	{
+		array[i] = min;
+		min = min + 1;
+	}
+	return (array);
 }
