d67f7380064b14e7770b54fcf487e26d20427721
themba <seconds.seconds1@gmail.com>
2023-02-10 11:32:55 -0800

    l3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,13 +9,13 @@
 
 int main(void)
 {
-	int i,j;
+	int i, j;
 
 	for (i = '0'; i <= '9'; i++)
 	{
 		for (j = '0'; j <= '9'; j++)
 		{
-			if ((i < j) & (i <= '9'))
+			if ((i < j) & (j <= '9'))
 			{
 				putchar(j);
 				putchar(i);
