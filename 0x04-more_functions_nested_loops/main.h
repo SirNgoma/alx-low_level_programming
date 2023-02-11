8bd8be13ae4b3b83a489c56ec5cfb21804a395a3
themba <seconds.seconds1@gmail.com>
2023-02-10 22:22:58 -0800

    u

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -2,6 +2,7 @@
 #define MAIN_H
 
 #include <stdio.h>
+
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
@@ -13,4 +14,6 @@ void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
 int _putchar(char c);
+void print_number(int n);
+
 #endif /* MAIN_H */
